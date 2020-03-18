f74ee5c9f43ace31aa9f68441a00a0e85e047786
Rowan Nicholls <rowan.nicholls@perspectum-diagnostics.com>
2020-03-18 20:11:40 +0000

    Edited the R co2/CO2 datasets as macOS does not recognise case differences

--- R/datasets/List of Files in Folder.py
+++ R/datasets/List of Files in Folder.py
@@ -4,14 +4,20 @@ import os
 #
 # CREATES LIST WITH THE NAMES OF THE FILES IN THE FOLDER
 #
-files = os.listdir()
-files.remove(os.path.basename(__file__))
+files = os.listdir('datasets')
 # REMOVE .rmd FILES
 for filename in list(files):
     if filename.endswith('.rmd'):
         files.remove(filename)
-files.remove('aaa_Pre-Loaded-Datasets - Wrapper.html')
+# REMOVE HIDDEN FILES
+hidden_files = [file for file in files if file.startswith('.')]
+if hidden_files:
+    files = list(set(files) - set(hidden_files))
 # FINISH
 files = sorted(files, key=str.lower)
 for file in files:
-    print('  <td style="width:150%;" valign="top"><iframe width=100% height="6000" src="' + file + '" seamless></iframe></td>')
+    print(
+        '  <td style="width:150%;" valign="top">' +
+        '<iframe width=100% height="6000" src="datasets/' + file +
+        '" seamless></iframe></td>'
+    )
