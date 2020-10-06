5a95d8ef683cc775c9ed3565953abfce8e02d726
rowan@papa <rowan.charles.nicholls@gmail.com>
2020-10-06 19:18:43 +0100

    Added another passwords.py file

--- python/utilities/passwords.py
+++ python/utilities/passwords.py
@@ -1,14 +1,16 @@
 """
 Generic strong passwords.
 
-Often when websites require strong passwords they want something that contains:
+Often when websites require a strong password they want something that contains:
 
 - Alphanumerics (numbers and letters)
-- At least ten characters
+- At least 12 characters
 - At least one lowercase character
 - At least one uppercase character
 - At least one special character
-- At least two digits
+- At least one digit
+- No three or more consecutive numbers
+- No three or more consecutive letters
 
 Here's a snippet that will do that:
 """
@@ -17,12 +19,14 @@ import string
 
 alphabet = string.ascii_letters + string.digits + string.punctuation
 while True:
-    password = ''.join(secrets.choice(alphabet) for i in range(10))
+    password = ''.join(secrets.choice(alphabet) for i in range(12))
     if (
         any(c.islower() for c in password) and
         any(c.isupper() for c in password) and
         any(c in string.punctuation for c in password) and
-        sum(c.isdigit() for c in password) >= 3
+        any(c.isdigit() for c in password) and
+        not any(all([password[i].isdigit(), password[i + 1].isdigit(), password[i + 2].isdigit()]) for i, c in enumerate(password[:-2])) and
+        not any(all([password[i] in string.ascii_letters, password[i + 1] in string.ascii_letters, password[i + 2] in string.ascii_letters]) for i, c in enumerate(password[:-2]))
     ):
         break
 print(password)
