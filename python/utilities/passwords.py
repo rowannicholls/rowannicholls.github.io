fddbfcb7a19d929a100782fbec4842e123e433df
rowan@papa <rowan.charles.nicholls@gmail.com>
2020-11-10 21:12:20 +0000

    Various minor changes

--- python/utilities/passwords.py
+++ python/utilities/passwords.py
@@ -1,7 +1,8 @@
 """
 Generic strong passwords.
 
-Often when websites require a strong password they want something that contains:
+Often when websites require a strong password they want something that
+contains:
 
 - Alphanumerics (numbers and letters)
 - At least 10 characters
@@ -19,14 +20,29 @@ import string
 
 alphabet = string.ascii_letters + string.digits + string.punctuation
 while True:
-    password = ''.join(secrets.choice(alphabet) for i in range(15))
+    password = ''.join(secrets.choice(alphabet) for i in range(12))
     if (
         any(c.islower() for c in password) and
         any(c.isupper() for c in password) and
         any(c in string.punctuation for c in password) and
         any(c.isdigit() for c in password) and
-        not any(all([password[i].isdigit(), password[i + 1].isdigit(), password[i + 2].isdigit()]) for i, c in enumerate(password[:-2])) and
-        not any(all([password[i] in string.ascii_letters, password[i + 1] in string.ascii_letters, password[i + 2] in string.ascii_letters]) for i, c in enumerate(password[:-2]))
+        not any(
+            all(
+                [
+                    password[i].isdigit(), password[i + 1].isdigit(),
+                    password[i + 2].isdigit()
+                ]
+            ) for i, c in enumerate(password[:-2])
+        ) and
+        not any(
+            all(
+                [
+                    password[i] in string.ascii_letters,
+                    password[i + 1] in string.ascii_letters,
+                    password[i + 2] in string.ascii_letters
+                ]
+            ) for i, c in enumerate(password[:-2])
+        )
     ):
         break
 print(password)
