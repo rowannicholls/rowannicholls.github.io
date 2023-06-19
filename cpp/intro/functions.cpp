c68d877a3725bb37d471ec91e617769a121f5d42
Rowan Nicholls <rowan.charles.nicholls@gmail.com>
2023-06-19 16:11:53 +0100

    Added scope

---
+++ cpp/intro/functions.cpp
@@ -0,0 +1,8 @@
+std::string my_function(std::string message) {
+    // Use the variable passed in
+    std::cout << message << "\n";
+    // Initialise a local variable
+    std::string reply = "Hello User";
+    // Return the local variable
+    return reply;
+}
