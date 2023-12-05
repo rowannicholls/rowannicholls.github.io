02abf180ce3e49e524ea3a5956154060a7eca3f5
Rowan Nicholls <rowan.charles.nicholls@gmail.com>
2023-12-05 12:26:09 +0000

    Updated the Python installation check script

--- python/intro/installation_check.py
+++ python/intro/installation_check.py
@@ -16,6 +16,7 @@ Works on:
 │ 2021-08-31 │ macOS Big Sur  │ Python 3.9.6  │ ...rsions/3.9/bin/python3.9 │
 │ 2022-01-08 │ Ubuntu 20.04   │ Python 3.9.5  │ /usr/bin/python3            │
 │ 2022-01-08 │ Ubuntu 20.04   │ Python 3.10.1 │ /usr/local/bin/python3      │
+│ 2023-12-05 │ Ubuntu 22.04   │ Python 3.12.0 │ /usr/local/bin/python3      │
 └────────────┴────────────────┴───────────────┴─────────────────────────────┘
 """
 import distro
@@ -24,70 +25,86 @@ from pip._internal.operations.freeze import freeze
 import platform
 import subprocess
 import sys
-import time
+from datetime import date
 import pwd
 
-# General implementation
-today = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+today = date.today()
 print(f'This script was run on {today}')
-print(f'This is a {platform.system()} machine')
-name = platform.node()
-print(f'Its name is {name}')
-if platform.system() == 'Linux':
-    # Check which versoin of Python you are running
+
+# Information about the computer
+core_os = platform.system()
+print(f'This is a {core_os} machine')
+if core_os == 'Linux':
+    # Check which version of Python you are running
     major = platform.python_version().split('.')[0]
     minor = platform.python_version().split('.')[1]
-    if (int(major) == 3) & (int(minor) < 7):
-        # Python 3.6 or lower
+    # Check which version of the distro package you have installed
+    distro_major = distro.__version__.split('.')[0]
+    distro_minor = distro.__version__.split('.')[1]
+    # Check which operating system you have
+    if (int(major) == 3) & (int(minor) <= 4):
+        # Python 3.4 or lower
         OS = platform.linux_distribution()[0]
         version = platform.linux_distribution()[1]
-    elif (int(major) == 3) & (int(minor) < 7):
-        # Python 3.7 to 3.9
+    elif (int(distro_major) <= 1) & (int(distro_minor) <=4):
+        # distro 1.4 or lower
         OS = distro.linux_distribution()[0]
         version = distro.linux_distribution()[1]
     else:
-        # Python 3.10
+        # distro 1.5.0 or higher
         OS = distro.name()
         version = distro.version()
     print(f'Its OS is {OS}')
     print(f'Its OS version is {version}')
-elif platform.system() == 'Windows':
-    OS = platform.release()
-    print(f'Its OS is {OS}')
-elif platform.system() == 'Darwin':
+elif core_os == 'Windows':
+    OS = platform.system()
+    version = platform.release()
+    print(f'Its OS version is {version}')
+elif core_os == 'Darwin':
     macOS_vers = {
         '10.11': 'El Capitan',
         '10.12': 'Sierra',
         '10.13': 'High Sierra',
         '10.14': 'Mojave',
         '10.15': 'Catalina',
-        '10.16': 'Big Sur',
+        '11': 'Big Sur',
+        '12': 'Monterey',
+        '13': 'Ventura',
+        '14': 'Sonoma',
     }
     OS = macOS_vers[platform.mac_ver()[0][:5]]
     print(f'Its OS is macOS {OS}')
     print(f'Its OS version is {platform.mac_ver()[0]}')
-py = platform.python_version()
-print(f'Its Python version is {py}')
-path = sys.executable
-print(f'Python is being run from {path}')
+name = platform.node()
+print(f'Its name is {name}')
 user = pwd.getpwuid(os.getuid())[0]
 print(f'The user is {user}')
+
+# Information about the Git repo
 process = subprocess.Popen(
     ['git', 'rev-parse', 'HEAD'], shell=False, stdout=subprocess.PIPE
 )
 git_hash = process.communicate()[0].strip()
 print(f'The git hash is {git_hash}')
+
+# Information about the Python installation
+py = platform.python_version()
+print(f'The Python version is {py}')
+path = sys.executable
+print(f'Python is being run from {path}')
 print('The installed packages are:')
 for requirement in freeze(local_only=True):
     print(requirement)
 
-# Specific stuff
+# Formatted output
 print('\n\n')
 if platform.system() == 'Linux':
+    print(f'Running with Python {py} on {name}, an {OS} {version} machine, ' +
+          f'by user {user}')
     print('\nWorks on:')
-    print('┌────────────┬────────────────┬───────────────┬──────────────────┐')
-    print(f'│ {today:10} │ {OS} {version}   │ Python {py:6} │ {path:16} │')
-    print('└────────────┴────────────────┴───────────────┴──────────────────┘')
+    print('┌────────────┬────────────────┬───────────────┬──' + '─' * 22 + '┐')
+    print(f'│ {today} │ {OS} {version}   │ Python {py:6} │ {path:16} │')
+    print('└────────────┴────────────────┴───────────────┴──' + '─' * 22 + '┘')
 elif platform.system() == 'Darwin':
     print(f'Running with Python {py} on {name}, a macOS {OS} machine, ' +
           f'by user {user}')
